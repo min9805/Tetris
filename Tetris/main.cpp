efc33135326c874e2689b44c6789939ec7276d14
min1925k@gmail.com <min1925k@gmail.com>
2022-06-30 17:53:44 +0900

    Initial Commit

---
+++ Tetris/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by Min on 2022/06/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
